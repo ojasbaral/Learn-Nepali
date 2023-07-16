8c626f00539665061fd22083e6aed6a14224fbf9
Ojas Baral <ojas.baral@gmail.com>
2023-07-16 01:03:08 -0600

    navabar completed

---
+++ src/pages/landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header'
+
+const Landing = () => {
+  return (
+    <div>
+        <Header></Header>
+    </div>
+  )
+}
+
+export default Landing
